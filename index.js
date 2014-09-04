cc76dbfd8dd8565e423d81b095865213eac9de77
carletex <oceanrdn@gmail.com>
2014-09-04 15:54:10 -0400

    Log-in at hackerschool. Added package.json

--- index.js
+++ index.js
@@ -1,40 +1,98 @@
 var https = require('https');
 var querystring = require('querystring');
+var request = require('request');
+var RSVP = require('rsvp');
+var read = require("read")
+
+
+// Enable cookies for requests
+var j = request.jar()
+var bodyequest = request.defaults({jar: j});
 
 var CALLBACK_URI = 'urn:ietf:wg:oauth:2.0:oob'
-
 // Auth server end-points
-var HS_BASE_URL = 'www.hackerschool.com'
+var HS_BASE_URL = 'https://www.hackerschool.com'
 var HS_AUTHORIZE_URL= '/oauth/authorize'
 var HS_ACCESS_TOKEN_URL= '/oauth/token'
 
 
-function getAuthToken() {
+function HSLogin (credentials) {
 
-	var query = {
-    'client_id': process.env.HS_CONSUMER_KEY,
-    'response_type': 'code',
-    'redirect_uri': CALLBACK_URI
-  }
+	// Get the Authenticity form token in /login
+	return new RSVP.Promise(function(resolve, reject) {
 
-	var options = {
-		hostname: HS_BASE_URL,
-		port: 443,
-		path: HS_AUTHORIZE_URL + '?' + querystring.stringify(query),
-	};
+		request.get(HS_BASE_URL + '/login', function(error, response, body) {
 
-	https.get(options, function(res) {
+			var regex = /<meta.*content="(.*?)".*name="csrf-token".*\/>/;
+			var authenticityToken = body.match(regex)[1];
 
-		console.log("Status Code:", res.statusCode);
-		console.log("Headers:", res.headers);
+			var params = {
+				'authenticity_token': authenticityToken,
+				'email': credentials.username,
+				'password': credentials.password,
+				'commit': 'Log+in'
+			}
 
-		res.on('data', function(data) {
-		  console.log('Response:', data.toString());
+			var query = {
+				url: HS_BASE_URL + '/sessions',
+				form: params
+			}
+
+			request.post(query, function(error, response, body) {
+				console.log('RES:', error, body);
+				resolve();
+			});
+
 		});
 
-	}).on('error', function(e) {
-  	console.error('Something went wrong:', e);
-	});;
+	});
+
 }
 
-getAuthToken();
+// function getAuthToken() {
+
+// 	var query = {
+//     'client_id': process.env.HS_CONSUMER_KEY,
+//     'response_type': 'code',
+//     'redirect_uri': CALLBACK_URI
+//   }
+
+// 	var options = {
+// 		url: HS_BASE_URL + HS_AUTHORIZE_URL + '?' + querystring.stringify(query),
+// 	};
+
+// 	request.get(options, function(error, response, body) {
+// 		console.log(error, response, body);
+
+// 	});
+// }
+
+function getHSCredentials() {
+	return new RSVP.Promise(function(resolve, reject){
+		var credentials = {};
+
+		return new RSVP.Promise(function(resolve, reject) {
+			read({prompt: 'HS username: '}, function(error, text) {
+				credentials.username = text;
+				resolve();
+			});
+		})
+		.then(function() {
+			read({prompt: 'HS password: ', silent: true}, function(error, text) {
+				credentials.password = text;
+				resolve(credentials);
+			});
+		});
+	});
+}
+
+
+// MAIN
+
+getHSCredentials()
+.then(function(credentials) {
+	return HSLogin(credentials);
+})
+.then(function(session) {
+
+});
